787fdf0fc04e8d1cdbc9fbbd7c3f65c539a698df
Jake Greene <jake.greene@razorfish.com>
2016-02-02 16:25:00 -0500

    Create A Solid Example
    This example uses the notion of a SKU (stock-keeping-unit)

--- src/main/scala/ca/jakegreene/spark/redis/Example.scala
+++ src/main/scala/ca/jakegreene/spark/redis/Example.scala
@@ -3,11 +3,28 @@ package ca.jakegreene.spark.redis
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import ca.jakegreene.spark.redis._
+import ca.jakegreene.spark.redis.rdd._
 
 object Example {
   def main(args: Array[String]): Unit = {
     val config = new SparkConf(true).setAppName("spark-redis-example")
     val sc = new SparkContext(config)
-    val hashes = sc.redisHashes("Sku-*")
+    val skusByKey = sc.redisHashes("Sku-*")
+    val skusByProductId = skusByKey
+      .map {
+        case (redisKey, sku) => (sku("product_id"), sku)
+      }
+      .groupByKey()
+    val productsByKey = sc.redisHashes("Prod-*")
+    val updatedProducts = productsByKey
+      .join(skusByProductId)
+      .map {
+        case (productId, (product, skus)) => (productId, rollup(product, skus.toList))
+      }
+    updatedProducts.saveToRedis()
+  }
+  
+  def rollup(product: Map[String, String], skus: Seq[Map[String, String]]): Map[String, String] = {
+    ???
   }
 }
