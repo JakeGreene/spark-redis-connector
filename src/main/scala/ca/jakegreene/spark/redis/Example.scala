fe255078fbea6a68bbcf7932ef0ef9b142de09f8
Jake Greene <jake.greene@razorfish.com>
2016-02-02 16:44:15 -0500

    Update Example To Cover Use-Case
    Show off the interface of the spark-redis connector.
    - Reading hashes from the SparkContext
    - Saving to Redis directly from an RDD

--- src/main/scala/ca/jakegreene/spark/redis/Example.scala
+++ src/main/scala/ca/jakegreene/spark/redis/Example.scala
@@ -3,11 +3,26 @@ package ca.jakegreene.spark.redis
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import ca.jakegreene.spark.redis._
+import ca.jakegreene.spark.redis.rdd._
 
 object Example {
   def main(args: Array[String]): Unit = {
     val config = new SparkConf(true).setAppName("spark-redis-example")
     val sc = new SparkContext(config)
-    val hashes = sc.redisHashes("Sku-*")
+    val childByKey = sc.redisHashes("Child:*")
+    val childByParentId = childByKey
+      .map {
+        case (childKey, child) => ("Parent:"+child("parent_id"), child)
+      }
+      .groupByKey()
+    val parentByKey = sc.redisHashes("Parent:*")
+    val updatedParents = parentByKey
+      .join(childByParentId)
+      .map {
+        case (parentKey, (parent, child)) => (parentKey, rollup(parent, child.toList))
+      }
+    updatedParents.saveToRedis()
   }
+  
+  def rollup(parent: Map[String, String], children: Seq[Map[String, String]]): Map[String, String] = ???
 }
